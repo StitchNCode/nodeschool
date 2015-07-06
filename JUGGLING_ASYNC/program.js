083b895725c364318d50362f6e06b20be2932ef8
Scality Hackathon Student <lvergero@student.42.fr>
2015-07-06 18:31:36 +0200

    fracking hw

--- JUGGLING_ASYNC/program.js
+++ JUGGLING_ASYNC/program.js
@@ -1,20 +1,54 @@
-var http = require('http')
+var http = require('http');
+var bl   = require('bl');
 
-var i = 2
 var print = []
 
-function get(){
-	http.get(process.argv[i], function callback(response){
-	var ret = ""
-	response.on('data', function(data){
-		ret += data
-		print.push(ret)	
-	})
-	response.on('end', function(data){
-		i += 1
-		console.log(print)
-		if (i < 5)
-			get();
+// http.get(process.argv[2], function callback(response)
+// 	{
+// 	var ret = ""
+// 	response.on('data', function(data){
+// 		ret += data
+// 	})
+// 	response.on('end', function(data){
+// 		print.push(ret)
+// 		http.get(process.argv[3], function callback(response_two)
+// 		{
+// 			var ret_two = ""
+// 			response_two.on('data', function(data)
+// 			{
+// 				ret_two += data
+// 			})
+// 			response_two.on('end', function(data)
+// 			{
+// 				print.push(ret_two)
+// 				http.get(process.argv[3], function callback(response_three)
+// 				{
+// 					var ret_three = ""
+// 					response_three.on('data', function(data)
+// 					{
+// 						ret_three += data
+// 					})
+// 					response_three.on('end', function(data)
+// 					{
+// 						print.push(ret_three)
+// 						for (var i = 0; i < print.length; i++)console.log(print)
+// 							console.log(print[i])
+// 					})
+// 				})
+// 			})
+// 		})
+// 	})
+// })
+
+http.get(process.argv[2], function(data){
+	print.push(data);
+	http.get(process.argv[3], function(data){
+		print.push(data);
+		http.get(process.argv[4], function(data){
+			print.push(data);
+			for (var i = 0; i < print.length; i++){
+				console.log(print[i])
+			}
+		});
 	})
 })
-}
